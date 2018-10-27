7b70bb0f484a952c20732f273544dfa96b9cfa9f
Harshit Thukral <h.thakral369@gmail.com>
2018-10-28 01:26:38 +0530

    build: create seperate bundle for each use-case

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,23 @@ import babel from 'rollup-plugin-babel';
 
 module.exports = {
   input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'umd',
-    name: 'await-here'
-  },
+  output: [
+    {
+      file: 'dist/await-here.cjs.js',
+      format: 'cjs',
+      name:'await-here'
+    },
+    {
+      file: 'dist/await-here.umd.js',
+      format: 'umd',
+      name:'await-here'
+    },
+    {
+      file: 'dist/await-here.esm.js',
+      format: 'esm',
+      name:'await-here'
+    }
+  ],
   plugins: [
     babel({
       exclude: 'node_modules/**' // only transpile our source code

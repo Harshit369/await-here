3593b631d5c7bb5494eafc4e28efef48db250ac3
Harshit Thukral <h.thakral369@gmail.com>
2019-01-27 14:53:33 +0530

    wip

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare const here: <T, E = any>(promise: Promise<T>) => Promise<[null, T] | [E, undefined]>;
+export default here;
--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 
 module.exports = {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
     {
       file: 'dist/await-here.cjs.js',
